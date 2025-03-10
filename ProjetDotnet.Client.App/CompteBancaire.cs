01722518d1fb8e9355827507c1964187750ea1e7
Roland AZOURY <roland>
2025-03-10 16:42:39 +0100

    add compte bancaire

---
+++ ProjetDotnet.Client.App/CompteBancaire.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjetDotnet.Client.App
+{
+    public class CompteBancaire
+    {
+        public string NuméroCompte { get; set; }
+        public DateTime DateOuverture { get; set; }
+        public decimal Solde { get; set; }
+
+        public CompteBancaire(string numéroCompte)
+        {
+            this.NuméroCompte = numéroCompte;
+            DateOuverture = DateTime.Now;
+            Solde = 1000.00m; 
+        }
+
+        public void AfficherCompte()
+        {
+            Console.WriteLine($"Numéro de Compte: {NuméroCompte}, Ouvert le: {DateOuverture.ToShortDateString()}, Solde: {Solde}€");
+        }
+
+    }
+}
