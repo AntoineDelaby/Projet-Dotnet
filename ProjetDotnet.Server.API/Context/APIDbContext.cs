4063c0e7ccc3b67ff456f09ae37e36d83b7124c4
Antoine Delaby <antoine.delaby30@gmail.com>
2025-03-10 11:59:10 +0100

    Ajout du contexte BDD

---
+++ ProjetDotnet.Server.API/Context/APIDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace ProjetDotnet.Server.API.Context
+{
+    public class APIDbContext : DbContext
+    {
+        public DbSet<Enregistrement> Historique { get; set; };
+        public DbSet<Enregistrement> HistoriqueErreurs { get; set; };
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=dbProj;Integrated Security=True;Connect Timeout=30;Encrypt=False;Trust Server Certificate=False;Application Intent=ReadWrite;Multi Subnet Failover=False");
+
+        }
+    }
+}
