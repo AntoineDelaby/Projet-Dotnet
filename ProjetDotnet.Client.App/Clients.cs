339f2c39c1ec5f918809115faa119ae935e515f1
Roland AZOURY <roland>
2025-03-10 19:27:44 +0100

    update clients

--- ProjetDotnet.Client.App/Clients.cs
+++ ProjetDotnet.Client.App/Clients.cs
@@ -11,20 +11,27 @@ namespace ProjetDotnet.Client.App
         public int Identifiant { get; set; }
         public string Nom { get; set; }
 
-        public string AdressePostal { get; set; }
+        public string Libelle_Adresse { get; set; }
+        public string Complement_Adresse { get; set; }
+        public string CodePostal { get; set; }
+        public string Ville { get; set; }
+
 
         public string Mail { get; set; }
 
 
 
-        public Clients(int identifiant, string nom, string adressepostal, string mail)
+        public Clients(int identifiant, string nom, string mail, string libelle_adresse,
+            string complement_adresse, string codepostal, string ville)
 
         {
             this.Identifiant = identifiant;
             this.Nom = nom;
-            this.AdressePostal = adressepostal;
             this.Mail = mail;
-
+            this.Libelle_Adresse = libelle_adresse;
+            this.Complement_Adresse = complement_adresse;
+            this.CodePostal = codepostal;
+            this.Ville = ville;
         }
 
 
@@ -50,10 +57,24 @@ namespace ProjetDotnet.Client.App
             this.Mail = mail;
         }
 
+        // Validation de l'adresse
+        public void SetAdresse(string libelle_adresse, string complement_adresse, string codePostal, string ville)
+        {
+            if (libelle_adresse.Length == 0 || codePostal.Length != 5 || ville.Length == 0)
+                throw new ClientsException(ClientsExceptionType.InvalidAdresse);
 
+            this.Libelle_Adresse = libelle_adresse;
+            this.Complement_Adresse = complement_adresse;
+            this.CodePostal = codePostal;
+            this.Ville = ville;
+        }
 
         public abstract void AfficherInfos();
+    
+
+        public void AfficherAdresse()
+        {
+            Console.WriteLine($"Adresse : {Libelle_Adresse}, {Complement_Adresse}, {CodePostal} {Ville}");
+        }
     }
-
-
 }
