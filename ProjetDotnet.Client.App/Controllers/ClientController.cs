0bc5db7c1ea9168dc19ab83e8b870b3601dbd656
Antoine Delaby <antoine.delaby30@gmail.com>
2025-03-12 12:01:51 +0100

    Adding database connection

--- ProjetDotnet.Client.App/Controllers/ClientController.cs
+++ ProjetDotnet.Client.App/Controllers/ClientController.cs
@@ -1,36 +1,32 @@
-﻿using ProjetDotnet.Server.Data;
+﻿using Microsoft.EntityFrameworkCore;
+using ProjetDotnet.Server.Data;
+using ProjetDotnet.Server.Data.Context;
+using ProjetDotnet.Server.Data.Migrations;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Text.Json;
 using System.Threading.Tasks;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.TextBox;
 
 namespace ProjetDotnet.Client.App.Controllers
 {
     class ClientController
     {
-        public ClientController() { }
+        public ClientController() {
+            InitializeDatabase();
+        }
 
-        // Récupère la liste des transactions effectuées par un compte bancaire
-        //public async Task<List<Historique>?> GetTransactionsById(List<CarteBancaire> cartes)
-        //{
-        //    // Chemin d'accès au fichier json
-        //    string projectRoot = Directory.GetParent(AppDomain.CurrentDomain.BaseDirectory).Parent.Parent.Parent.FullName;
-        //    string solutionRoot = Directory.GetParent(projectRoot).FullName;
-        //    string filePath = Path.Combine(solutionRoot, "ProjetDotnet.Client.App", "Json\\export.json");
+        // Initialisation de la base de données
+        private void InitializeDatabase()
+        {
+            //using var context = new ClientDbContext();
+            //context.Database.EnsureCreated();
+        }
 
-        //    using (FileStream fs = new FileStream(filePath, FileMode.Open, FileAccess.Read))
-        //    {
-        //        List<Historique>? historique = await JsonSerializer.DeserializeAsync<List<Historique>>(fs);
-
-        //        return historique
-        //            .Where<Historique>(h => cartes.Where<CarteBancaire>(c => c.NumeroCarte.Equals(h.NumCarte)))
-        //            .ToList<Historique>();
-        //    }
-        //}
-
-        public async Task<List<Historique>?> GetTransactionsBetweenDates(int id, DateTime begDate, DateTime endDate)
+        //Récupère la liste des transactions effectuées par un compte bancaire
+        public async Task<List<Historique>?> GetTransactionsById(List<CarteBancaire> cartes)
         {
             // Chemin d'accès au fichier json
             string projectRoot = Directory.GetParent(AppDomain.CurrentDomain.BaseDirectory).Parent.Parent.Parent.FullName;
@@ -42,9 +38,36 @@ namespace ProjetDotnet.Client.App.Controllers
                 List<Historique>? historique = await JsonSerializer.DeserializeAsync<List<Historique>>(fs);
 
                 return historique
-                    .Where<Historique>(h => h.Id == id && h.DateOperation > begDate && h.DateOperation < endDate)
+                    .Where<Historique>(h => cartes.Any(c => c.NumeroCarte == h.NumCarte))
                     .ToList<Historique>();
             }
         }
+
+        public async Task<List<Historique>?> GetTransactionsBetweenDates(List<CarteBancaire> cartes, DateTime begDate, DateTime endDate)
+        {
+            // Chemin d'accès au fichier json
+            string projectRoot = Directory.GetParent(AppDomain.CurrentDomain.BaseDirectory).Parent.Parent.Parent.FullName;
+            string solutionRoot = Directory.GetParent(projectRoot).FullName;
+            string filePath = Path.Combine(solutionRoot, "ProjetDotnet.Client.App", "Json\\export.json");
+
+            using (FileStream fs = new FileStream(filePath, FileMode.Open, FileAccess.Read))
+            {
+                List<Historique>? historique = await JsonSerializer.DeserializeAsync<List<Historique>>(fs);
+                return historique
+                    .Where<Historique>(h => cartes.Any(c => c.NumeroCarte == h.NumCarte)
+                                        && h.DateOperation > begDate 
+                                        && h.DateOperation < endDate)
+                    .ToList<Historique>();
+            }
+        }
+
+        //public async Task<List<Clients>> getAll()
+        //{
+        //    using var context = new APIDbContext();
+        //    var historique = await context.Historique
+        //        .ToListAsync<Historique>();
+
+        //    return historique;
+        //}
     }
 }
