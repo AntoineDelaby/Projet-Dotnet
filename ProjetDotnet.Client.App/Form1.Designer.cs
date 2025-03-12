98facb91f48c37b111a432a4001f0f016ce37da3
Nadia Schwaller <nadia1.schwaller@gmail.com>
2025-03-12 09:54:16 +0100

    Mise en place du systeme de connexion, sans verification

--- ProjetDotnet.Client.App/Form1.Designer.cs
+++ ProjetDotnet.Client.App/Form1.Designer.cs
@@ -107,6 +107,7 @@
             button1.TabIndex = 5;
             button1.Text = "Se connecter";
             button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
             // 
             // textBox2
             // 
--- ProjetDotnet.Client.App/Form1.cs
+++ ProjetDotnet.Client.App/Form1.cs
@@ -12,12 +12,27 @@ namespace ProjetDotnet.Client.App
 {
     public partial class Form1 : Form
     {
+        private TabPage hiddenCompte;
+        private TabPage hiddenJSON;
+        private TabPage hiddenDate;
         public Form1()
         {
             InitializeComponent();
-            //tabPage2.Hide();
-            //tabPage3.Hide();
-            //tabPage4.Hide();
+            hiddenDate = tabControl1.TabPages[3];
+            tabControl1.TabPages.Remove(tabControl1.TabPages[3]);
+
+            hiddenJSON = tabControl1.TabPages[2];
+            tabControl1.TabPages.Remove(tabControl1.TabPages[2]);
+
+            hiddenCompte = tabControl1.TabPages[1];
+            tabControl1.TabPages.Remove(tabControl1.TabPages[1]);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            tabControl1.TabPages.Insert(1, hiddenCompte);
+            tabControl1.TabPages.Insert(2, hiddenJSON);
+            tabControl1.TabPages.Insert(3, hiddenDate);
         }
     }
 }
